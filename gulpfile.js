6907f01d055522ee9a59850f2e5ccb40b39e5dc9
Kyle Stetz <kyle@punkave.com>
2014-10-11 14:05:50 -0400

    updated tests

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,6 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 
 gulp.task('test', function () {
-  return gulp.src('tests/main.js', { read: false })
+  gulp.src('tests/main.js', { read: false })
     .pipe( mocha({ }) );
 });
