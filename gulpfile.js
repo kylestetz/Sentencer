c63abd34c35d688d3e276b9b0c1ff0acd8f99d9c
Kyle Stetz <kyle@punkave.com>
2015-02-08 00:22:09 -0500

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,42 @@
 var gulp = require('gulp');
+var less = require('gulp-less-sourcemap');
+var markdown = require('gulp-markdown');
+var path = require('path');
+var fs = require('fs');
+var cheerio = require('cheerio');
 
-// ==================================================================
-//  TEST
-// ==================================================================
-
-var mocha = require('gulp-mocha');
-
-gulp.task('test', function () {
-  gulp.src('tests/main.js', { read: false })
-    .pipe( mocha({ }) );
+gulp.task('less', function () {
+	gulp.src('./css/site.less')
+	.pipe(less({
+		sourceMap: {
+			sourceMapRootpath: './' // Optional absolute or relative path to your LESS files
+		}
+	}))
+  .on('error', function (error) {
+    console.error(error);
+  })
+	.pipe(gulp.dest('./css'));
 });
+
+
+gulp.task('md', function () {
+    return gulp.src('README.md')
+      .pipe(markdown())
+      .pipe(gulp.dest('./'))
+    ;
+});
+
+gulp.task('inject', ['md'], function (callback) {
+	fs.readFile('./index.html', { encoding: 'utf8' }, function(err, indexHtml) {
+		fs.readFile('./README.html', { encoding: 'utf8' }, function(err, readmeMarkup) {
+
+			var $ = cheerio.load(indexHtml);
+      $('#readme').html(readmeMarkup);
+
+      fs.writeFileSync('index.html', $.html());
+      return callback(null);
+		});
+	});
+});
+
+gulp.task('default', ['less', 'md', 'inject']);
---
+++ js/site.js
@@ -0,0 +1,41 @@
+$( function() {
+
+  var $input  = $('[data-input]');
+  var $output = $('[data-output]');
+  var $render = $('[data-render]');
+  var $spookyRender = $('[data-spooky-render]');
+
+  //////////////////////////////////
+
+  function renderText(template, spooky) {
+    $.post(
+      'http://localhost:3000/' + (spooky ? 'spooky' : 'sentencer'),
+      { template: template },
+      function(data) {
+        $output.html(data);
+      }
+    );
+  }
+
+  //////////////////////////////////
+
+  $input.on('keypress', function(e) {
+    if(e.which === 13) {
+      e.preventDefault();
+      renderText($input.html());
+    }
+  });
+
+  $render.on('click', function(e) {
+    renderText($input.html());
+  });
+
+  $spookyRender.on('click', function(e) {
+    renderText($input.html(), true);
+  });
+
+  //////////////////////////////////
+
+  renderText($input.html());
+
+});
