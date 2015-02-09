361e556b3c26c4655e4c765426a08dcd4e6068e7
Kyle Stetz <kyle@punkave.com>
2015-02-09 08:47:37 -0500

    added responsive styles and favicon

--- js/site.js
+++ js/site.js
@@ -1,17 +1,34 @@
 $( function() {
 
+  var tap = !!('ontouchstart' in window) || !!('msmaxtouchpoints' in window.navigator);
+  $('body').addClass(tap ? 'tap' : 'no-tap');
+
+  //////////////////////////////////
+
   var $input  = $('[data-input]');
   var $output = $('[data-output]');
   var $render = $('[data-render]');
   var $spookyRender = $('[data-spooky-render]');
 
+  var makingRequest = false;
+
   //////////////////////////////////
 
   function renderText(template, spooky) {
+    // no ajaxing if we're already ajaxing.
+    if(makingRequest) {
+      return;
+    }
+    // dim the output to indicate that we're waiting
+    makingRequest = true;
+    $output.toggleClass('dim', true);
+
     $.post(
       'http://kylestetz.com/' + (spooky ? 'spooky' : 'sentencer'),
       { template: template },
       function(data) {
+        makingRequest = false;
+        $output.toggleClass('dim', false);
         $output.html(data);
       }
     );
